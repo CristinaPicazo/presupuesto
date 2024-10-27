810a37d100765d4e2155e6484a5c1fd35babaf9f
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-10-27 21:47:42 +0100

    actualizar presupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,25 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(presupuesto) {
+  if ((typeof presupuesto === "number") & (presupuesto > 0)) {
+    return presupuesto;
+  } else {
+    return "Tu presupuesto actual es de 0 €";
+  }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+  // TODO
 }
 
 function CrearGasto() {
-    // TODO
+  // TODO
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
-}
+export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
